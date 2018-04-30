573cd79121fcc22c91a693636076db7476d25f43
Robert Shaw <robertshaw87@gmail.com>
2018-04-30 00:08:17 -0700

    create framework html, add minor initial styling to page

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// game reset function
+//  reset values to default
+    // generate random value for the crystal
+    // generate 4 new gems with one guaranteed to have a value of 1
+// update displays
+
+// listen to click on gems
+// subtract value of clicked gem from crystal
+// if crystal is 0, win
+// if crystal is less than 0, lose
+// else, update crystal value
+
+// if reset button clicked, reset values to default
